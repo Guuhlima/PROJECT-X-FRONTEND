fa930057a21b0c2eb8753be5210aa52bf37e08fa
Gustavo <guuhalves0922@gmail.com>
2026-01-08 22:15:29 -0300

    create page register

--- tracking-front/app/(auth)/login/page.tsx
+++ tracking-front/app/(auth)/login/page.tsx
@@ -4,38 +4,28 @@ import Link from "next/link"
 
 export default function Page() {
     return (
-        <div >
-            <div className="flex flex-col gap-2 items-center">
-                Login Page
-            </div>
+        <div className="flex justify-end mt-20">
+            <div className="flex flex-col w-105 mt-20 mr-44 gap-4">
+                <div className="flex flex-col items-center">
+                    <span className="text-lg font-semibold">Tracking Your's Products</span>
+                </div>
 
-            <div className="flex justify-end mt-20">
-                <div className="flex flex-col w-150 h-150 mt-20 mr-44 gap-4">
-                    <span className="ml-52">
-                        Tracking Your's Products
+                <div className="flex flex-col gap-2">
+                    <Input className="w-full" placeholder="Username" />
+                    <Input className="w-full" type="password" placeholder="Password" />
+                </div>
+
+                <Button className="w-full" type="submit">
+                    Sign In
+                </Button>
+
+                <div>
+                    <span>
+                        Don't have an account?{" "}
+                        <Link href="/register" className="text-blue-500 hover:underline">
+                            Sign Up
+                        </Link>
                     </span>
-                    
-                    <div>
-                        <Input className="mb-2" placeholder="Username" />
-                        <Input type="password" placeholder="Password"/>
-                    </div>
-
-                    <div>
-                        <div className="">
-                            <Button className="w-full" type="submit">
-                                Sing In
-                            </Button>
-                        </div>
-                    </div>
-
-                    <div className="">
-                        <span>
-                            Don't have an account?{" "}
-                            <Link href="/register" className="text-blue-500 hover:underline">
-                                Sing Up
-                            </Link>
-                        </span>
-                    </div>
                 </div>
             </div>
         </div>
--- tracking-front/app/(auth)/register/page.tsx
+++ tracking-front/app/(auth)/register/page.tsx
@@ -1,7 +1,33 @@
+import Button from "@/app/shared/components/Button"
+import Input from "@/app/shared/components/Input"
+import Link from "next/link"
+
 export default function Page() {
     return (
-        <div>
-            Hello World
+        <div className="flex justify-end mt-20">
+
+            <div className="flex flex-col w-105 mt-20 mr-44 gap-4">
+                <div className="flex flex-col items-center">
+                    <span className="text-lg font-semibold">Create Your Account</span>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                    <Input className="w-full" type="text" placeholder="Username" />
+                    <Input className="w-full" type="email" placeholder="Email" />
+                    <Input className="w-full" type="password" placeholder="Password" />
+                </div>
+
+                <Button type="submit">Submit</Button>
+
+                <div>
+                    <span>
+                        You have Account?{" "}
+                        <Link href="/login" className="text-blue-500 hover:underline">
+                            Sing In
+                        </Link>
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
