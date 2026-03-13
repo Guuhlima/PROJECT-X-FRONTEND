9f74254300a1c6a408c1e421521472cac3b63714
Gustavo <guuhalves0922@gmail.com>
2026-03-12 23:07:40 -0300

    warehouse(feat): create documents from warehouse

--- app/shared/data/Items.ts
+++ app/shared/data/Items.ts
@@ -9,21 +9,21 @@ export const rawItems = [
       ],
     },
     {
-      label: "Profile",
+      label: "Transfer",
       bgColor: "#170D27",
       textColor: "#fff",
       links: [
-        { label: "Tracks", ariaLabel: "Featured Projects" },
-        { label: "Case Studies", ariaLabel: "Project Case Studies" },
+        { label: "Receipt", ariaLabel: "Featured Projects" },
+        { label: "Scheduling", ariaLabel: "Project Case Studies" },
       ],
     },
     {
-      label: "Contact",
+      label: "Warehouse",
       bgColor: "#271E37",
       textColor: "#fff",
       links: [
-        { label: "Email", ariaLabel: "Email us" },
-        { label: "Twitter", ariaLabel: "Twitter" },
+        { label: "Stocks", ariaLabel: "Email us" },
+        { label: "Location", ariaLabel: "Twitter" },
         { label: "LinkedIn", ariaLabel: "LinkedIn" },
       ],
     },
@@ -35,13 +35,6 @@ const toSlug = (s: string) =>
 export const items = rawItems.map((section) => ({
     ...section,
     links: section.links.map((l) => {
-      if (l.label === "Twitter") {
-        return {
-          ...l,
-          href: "https://twitter.com/seu_usuario",
-          external: true,
-        };
-      }
       if (l.label === "LinkedIn") {
         return {
           ...l,
